9b91a1e61610f5210166948da947e64e993afce8
preeja33 <preeja_raveendran@comcast.com>
2026-01-27 10:01:58 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -6,7 +6,7 @@ GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 ############################
 # Build entservices-ledcontrol
-echo "buliding entservices-ledcontrol"
+echo "building entservices-ledcontrol"
 
 cd ${GITHUB_WORKSPACE}
 cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-ledcontrol \
